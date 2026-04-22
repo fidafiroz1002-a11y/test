bd533389e17d30f368c21dd1faaff53143d06172
Fida <fidafiroz1002@gmail.com>
2026-04-22 14:19:57 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1=float(input("enter the number"))
+num2=float(input("enter the number"))
+op = input("enter operation")
+if op == '+':
+   print("result=",num1+num2)
+elif op == "-":
+   print("result=",num1-num2)
+elif op == "*":
+   print("result=",num1*num2)
+elif op == "/":
+   print("result=",num1/num2)
+else:
+   print("invalid")
