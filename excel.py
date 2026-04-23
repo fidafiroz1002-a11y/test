2167d757bf5ce67105d94a949263225f37ef65be
Fida <fidafiroz1002@gmail.com>
2026-04-23 10:22:17 +0530

    initial commit

---
+++ excel.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("marks.csv")
+
+print(df)
+
+plt.bar(df["names"],df["marks"])
+plt.xlabel("Students")
+plt.ylabel("Marks")
+plt.title("Student Marks Graph")
+plt.show()
