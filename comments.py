725e28334b24497a403113f802f027510e14d325
Fida <fidafiroz1002@gmail.com>
2026-04-29 15:54:29 +0530

    initial

---
+++ comments.py
@@ -0,0 +1,17 @@
+# a variable name should start with a letter or an underscore character
+# a variable name should not start with a number
+# a variable name should can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# a variable name cannot be any of the Python keywords.
+# encapsulatiom and polymorphism
+# wrapping up of ata and function into a single entity
+# restricts access to variables and methods from outside
+# prevents data accidental or unauthorised modification
+# provides securiy by hiding data from outside world
+# git
+# git init- initialization
+# git add- add files
+# git commit -m "initial commit"
+# git remote add origin <repo_url>
+# git branch-M main
+# git push -u origin main
